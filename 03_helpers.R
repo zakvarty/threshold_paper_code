adfe1ddd93178ef1bad162ae3956ec65f8d26c38
Zak D Varty <44709288+zakvarty@users.noreply.github.io>
2025-08-19 18:09:16 +0100

    clean motivating example code

---
+++ 03_helpers.R
@@ -0,0 +1,65 @@
+#helper functions for motivating example
+
+compare_MSEs <- function(MSEs, bar_colors = NULL, bar_names = NULL, legend_position = 'topleft', legend_text = letters[seq_along(MSEs)], legend_cex = 1,...){
+
+  # Function to plot many MSE decompositions side-by-side
+  heights <- NULL
+  if(is.null(bar_names)){ bar_names <- c('bais^2(sigma)', 'bais^2(xi)', 'var(sigma)','var(xi)')}
+  if(is.null(bar_colors)){ bar_colors <- grey(ppoints(length(MSEs))) }
+
+  for(i in seq_along(MSEs)){
+    heights_i <- c(MSEs[[i]]$sq_bias, MSEs[[i]]$variance)
+    heights <- rbind(heights,heights_i)
+  }
+
+  barplot(height = heights, beside = TRUE, names.arg = bar_names, col = bar_colors, ...)
+  legend(legend_position, legend = legend_text, col = bar_colors, pch = 15, bty = 'n', cex = legend_cex)
+
+  heights_df <- as.data.frame(heights, row.names = legend_text)
+  colnames(heights_df) <- bar_names
+  invisible(heights_df)
+}
+
+
+mle_return_levels <- function(mle_obj, ...){
+  out <- purrr::pmap(
+    .l = list(scale = mle_obj$sig_u, shape = mle_obj$xi),
+    .f = return_level,
+    period = return_periods,
+    shift = u,
+    v = v_cons[1])
+
+  # reformatting output
+  out <- do.call(rbind, out)
+  colnames(out) <- stringr::str_c(return_periods)
+
+  as_numeric_factor <- function(x) {as.numeric(levels(x))[x]}
+
+  out %>% as.data.frame() %>%
+    tibble::rownames_to_column("boot_id") %>%
+    tidyr::pivot_longer(-boot_id,
+                        names_to = "return_period",
+                        values_to = "return_level") %>%
+    mutate(
+      boot_id = as.numeric(boot_id),
+      return_period = as.numeric(return_period))
+}
+
+
+
+return_level_confidence_interval<- function(rl_obj, alpha = 0.05){
+  rl_obj %>%
+    mutate(return_period = as.factor(return_period)) %>%
+    summarise(
+      lower_limit = unname(quantile(return_level, alpha / 2)),
+      median = unname(quantile(return_level, 0.5)),
+      upper_limit = unname(quantile(return_level, 1 - alpha / 2)),
+      .by = return_period) %>%
+    mutate(return_period = as_numeric_factor(return_period))
+}
+
+CI_lines <- function (CI_obj, ...) {
+  lines(x = CI_obj$return_period, y = CI_obj$median, ...)
+  lines(x = CI_obj$return_period, y = CI_obj$lower_limit, ...)
+  lines(x = CI_obj$return_period, y = CI_obj$upper_limit, ...)
+}
--- 03_variable_threshold_benefits/motivating_example.R
+++ 03_variable_threshold_benefits/motivating_example.R
@@ -2,38 +2,45 @@
 # Small simulation study to show the benefits of stepped threshold
 # Forms section 3 of paper on threshold selection
 #
-# For conservative, stepped and extended catalouges show:
-# parameter recovery, mse decomposition and conditional return level estimation
+# For conservative, stepped and extended catalogues show:
+#  - parameter recovery,
+#  - mse decomposition,
+#  - conditional return level estimation
 ##
 
-# 1: Source necessary code -----------------------------------------------------
-source('../00_src/_gpd.R')
-source('../00_src/round_to_nearest.R')
-source('../00_src/llh_gpd_rd_varu.R')
-source('../00_src/mle_gpd_rd_varu.R')
-source('../00_src/variable_threshold_benefits/split_MSE.R')
+SEED = 1234
+DELTA = 0.1
+N_SIMS = 1000
 
+OUTPUT_PATH <- here("00_outputs", "03_motivating_example")
+
+# 1: Source necessary code -----------------------------------------------------
 library(dplyr)
+library(here)
+library(threshold)
+source("03_helpers.R")
 
 # 2: Simulate example catalogue ------------------------------------------------
-
+set.seed(SEED)
 ## 2.1: Set underlying parameters ----------------------------------------------
 cat_size <- 1000
 u <- 1.05
 sig_u <- 0.3
 xi <- 0.1
-to_nearest <- 0.1
+to_nearest <- DELTA
 v_cons <- rep(1.65, cat_size)
 v_step <- rep(c(1.65, 1.05), each = cat_size / 2)
 
-## 2.2: Create full catagloue --------------------------------------------------
+
+## 2.2: Create full catalogue --------------------------------------------------
 mags_full <- rgpd_rd(
   n = cat_size,
-  mu_latent = u,
-  sig = sig_u,
-  xi =  xi,
+  shift_latent = u,
+  scale = sig_u,
+  shape =  xi,
   to_nearest = to_nearest
 )
+
 cat_full <- data.frame(
   index = seq_along(mags_full),
   mag = mags_full,
@@ -41,10 +48,8 @@ cat_full <- data.frame(
   v_step = v_step
 )
 
-## 2.3: Thin by censoring and model inclusion ----------------------------------
-cat_censored <- filter(cat_full, mag >= v_step)
-cat_step <- cat_censored
-cat_cons <- filter(cat_censored, mag >= v_cons)
+cat_step <- filter(cat_full, mag >= v_step)
+cat_cons <- filter(cat_full, mag >= v_cons)
 
 ## 2.4: Plot catalogues --------------------------------------------------------
 plot_catalogue <- function(cat, main =""){
@@ -60,77 +65,80 @@ plot_catalogue <- function(cat, main =""){
 }
 
 ## Plot distinctly the catalogues that:
-# Happened
+
 plot_catalogue(cat_full, main = paste0(nrow(cat_full),' happened'))
-# were observed
-plot_catalogue(cat_censored, main = paste0(nrow(cat_censored),' observed'))
-# were above the conservative threshold
+
 plot_catalogue(cat_cons, main = paste0(nrow(cat_cons),' in conservative model'))
-lines(x = cat_cons$index, y = cat_cons$v_cons, col = 2, lwd = 1.5)
-# were above the stepped theshold
+lines(x = cat_cons$index, y = cat_cons$v_cons, col = "purple", lwd = 2)
+
 plot_catalogue(cat_step, main = paste0(nrow(cat_step),' in stepped model'))
-lines(x = cat_step$index, y = cat_step$v_step, col = 2, lwd = 1.5)
+lines(x = cat_step$index, y = cat_step$v_step, col = "darkorange", lwd = 2)
 
 ## Plot these but overlaid (USED IN PAPER)
-pdf(file = "./output/plots/cat_plot_motivating_example.pdf",width = 8,height = 4)
+file_name <- "catalogue_plot_motivating_example.pdf"
+path <- here(OUTPUT_PATH, file_name)
+pdf(file = path, width = 8, height = 4)
 par(mar = c(4.1,4.1,2.1,2.1))
-color <- if_else(cat_full$mag >= v_step, true = 'black', false = "grey50")
+color <- if_else(cat_full$mag >= v_step, true = 'grey60', false = "grey80")
 plot(x = cat_full$index,
-     y = cat_full$mag, pch = 16,
+     y = cat_full$mag,
+     pch = 16,
      cex = 0.8,
      col = color,
      bty = 'n',
      ylim = c(1,4),
      xlab = 'event index',
      ylab = 'magnitude')
-lines(x = c(0,500.5,500.5,1000), y = c(1.65, 1.65,1.05,1.05), lwd = 2)
-lines(x = c(0,1000), y = c(1.65,1.65), lty = 2, col = 2, lwd = 2)
+lines(x = c(0,1000), y = c(1.65,1.65), lwd = 2, col = "darkorange")
+lines(x = c(0,500.5,500.5,1000), y = c(1.65, 1.65,1.05,1.05), lty = 2, lwd = 2, col = "black")
 dev.off()
 
 
 # 3: Obtain mles for many conservative, stepped and extended catalogues  -------
 
-#NB: extended catalgoues have n_extended = n_stepped but all above v_conserative
+#NB: extended catalogues have n_extended = n_stepped but all above v_conserative
 
 ## 3.1: Set up true values and storage -----------------------------------------
-n_sims <- 1000
+
 sig_v_cons <- sig_u + (v_cons - u) * xi
 sig_v_step <- sig_u + (v_step - u) * xi
 
-mles_cons <- data.frame(sig_u = rep(NA, n_sims), xi = rep(NA, n_sims))
-mles_step <- data.frame(sig_u = rep(NA, n_sims), xi = rep(NA, n_sims))
-mles_xtra <- data.frame(sig_u = rep(NA, n_sims), xi = rep(NA, n_sims))
+mles_cons <- data.frame(sig_u = rep(NA, N_SIMS), xi = rep(NA, N_SIMS))
+mles_step <- data.frame(sig_u = rep(NA, N_SIMS), xi = rep(NA, N_SIMS))
+mles_xtra <- data.frame(sig_u = rep(NA, N_SIMS), xi = rep(NA, N_SIMS))
 
 # 3.2: Find conservative, stepped and extended data set mles -------------------
-for(i in 1:n_sims){
+
+for (i in 1:N_SIMS) {
+
   # Generate magnitudes for all earthquakes that happened
   mags_full <- rgpd_rd(
     n = cat_size,
-    mu_latent = u,
-    sig = sig_u,
-    xi =  xi,
+    shift_latent = u,
+    scale = sig_u,
+    shape =  xi,
     to_nearest = to_nearest)
 
-  # Make full, conservative and stepped catalouges
+  # Make full, conservative and stepped catalogues
   cat_full <- data.frame(index = seq_along(mags_full),
                          mag = mags_full,
                          v_cons = v_cons,
                          v_step = v_step)
-  cat_cons <- filter(cat_full, mag >= v_cons)
-  cat_step <- filter(cat_full, mag >= v_step)
+  cat_cons <- filter(cat_full, mag > v_cons)
+  cat_step <- filter(cat_full, mag > v_step)
 
   # Simulate some extra earthquakes for the extended catalogue
   n_extra <- nrow(cat_step) - nrow(cat_cons)
   mags_extra <- rgpd_rd(
     n = n_extra,
-    sig = sig_v_cons[1],
-    xi = xi,
-    mu_latent = v_cons[1],
+    scale = sig_v_cons[1],
+    shape = xi,
+    shift_latent = v_cons[1],
     to_nearest = to_nearest)
   v_extra <- rep(v_cons[1], n_extra)
 
   # Record the estimated parameters based on each threshold
-  mles_cons[i,] <- mle_gpd_rd_varu(
+  mles_cons[i,] <- mle_gpd_rd(
     x = cat_cons$mag,
     v = cat_cons$v_cons,
     sigxi = c(1,0),
@@ -140,7 +148,7 @@ for(i in 1:n_sims){
     hessian = FALSE
   )
 
-  mles_step[i,] <- mle_gpd_rd_varu(
+  mles_step[i,] <- mle_gpd_rd(
     x = cat_step$mag,
     v = cat_step$v_step,
     sigxi = c(1,0),
@@ -150,7 +158,7 @@ for(i in 1:n_sims){
     hessian = FALSE
   )
 
-  mles_xtra[i,] <- mle_gpd_rd_varu(
+  mles_xtra[i,] <- mle_gpd_rd(
     x = c(cat_cons$mag, mags_extra),
     v = c(cat_cons$v_cons, v_extra),
     sigxi = c(1,0),
@@ -159,29 +167,33 @@ for(i in 1:n_sims){
     llh_val = FALSE,
     hessian = FALSE
   )
-  if(i %%10 == 0) print(i)
+  if (i %% 10 == 0) print(i)
 }
 
 # 4: Plots relating to estimated parameters   ----------------------------------
 
 ## 4.1: parameter recovery plot ------------------------------------------------
-pdf("./output/plots/mle_values_motivating_example.pdf", width = 5, height = 5)
+
+file_name <- "bootstrap_mles_motivating_example.pdf"
+path <- here(OUTPUT_PATH, file_name)
+
+pdf(path, width = 5, height = 5)
 par(mar = c(4.5,4.5,1.0,1.0))
 plot(
   rbind(mles_cons, mles_step, mles_xtra),
   type = 'n',
   xlab = expression(hat(sigma)[u]),
   ylab = expression(hat(xi)), bty = 'n')
-points(mles_cons, pch = 16, cex = 0.8 , col = "gray30") #darkest
-points(mles_xtra, pch = 16, cex = 0.8 , col = "gray70") #lightest
-points(mles_step, pch = 16, cex = 0.8 , col = "gray60")
-points(x = sig_u, y = xi, pch = "+")
-legend('topright',
-       c("Conservative", "Extended", "Stepped", "True"),
-       col = c("grey30", "grey70", "grey60","black"),
-       pch = list(16,16,16,43),
-       cex = rep(1.2,4),
-       bty = 'n')
+  points(mles_cons, pch = 16, cex = 0.8 , col = "gray30") #darkest
+  points(mles_xtra, pch = 16, cex = 0.8 , col = "gray60") #lightest
+  points(mles_step, pch = 16, cex = 0.8 , col = "gray70")
+  points(x = sig_u, y = xi, pch = "+")
+  legend('topright',
+         c("Conservative", "Extended", "Stepped", "True"),
+         col = c("grey30", "grey60", "grey70","black"),
+         pch = list(16,16,16,43),
+         cex = rep(1.2,4),
+         bty = 'n')
 dev.off()
 
 ## 4.2: MSE decomposition plot -------------------------------------------------
@@ -210,33 +222,13 @@ MSE_xtra <- split_MSE(
 
 # Create plot to compare the MSE decompositions under each threshold
 
-compare_MSEs <- function(MSEs, bar_colors = NULL, bar_names = NULL, legend_position = 'topleft', legend_text = letters[seq_along(MSEs)], legend_cex = 1,...){
-  # Function to plot many MSE decompositions side-by-side
-  heights <- NULL
-  if(is.null(bar_names)){ bar_names <- c('bais^2(sigma)', 'bais^2(xi)', 'var(sigma)','var(xi)')}
-  if(is.null(bar_colors)){ bar_colors <- grey(ppoints(length(MSEs))) }
+file_name <- "mse_decomposition_motivating_example.pdf"
+path <- here(OUTPUT_PATH, file_name)
 
-  for(i in seq_along(MSEs)){
-    heights_i <- c(MSEs[[i]]$sq_bias, MSEs[[i]]$variance)
-    heights <- rbind(heights,heights_i)
-  }
-
-  barplot(height = heights, beside = TRUE, names.arg = bar_names, col = bar_colors, ...)
-  legend(legend_position, legend = legend_text, col = bar_colors, pch = 15, bty = 'n', cex = legend_cex)
-
-  heights_df <- as.data.frame(heights, row.names = legend_text)
-  colnames(heights_df) <- bar_names
-  invisible(heights_df)
-}
-
-
-pdf(file = "output/plots/mse_decomposition_motivating_example.pdf",
-    width = 5,
-    height = 5)
+pdf(file = path, width = 5, height = 5)
 par(mar = c(4.5,4.5,1.0,1.0))
 MSE_df <- compare_MSEs(
   MSEs = list(MSE_cons, MSE_xtra, MSE_step),
-  #ylim = c(0,0.003),
   legend_text = c("Conservative", "Extended", "Stepped"),
   legend_cex =  1.2,
   bar_names = c( expression(bias^2*(hat(sigma)[u])),
@@ -248,87 +240,67 @@ MSE_df <- compare_MSEs(
 )
 dev.off()
 
+
 ## 4.3: Calculate MSE with each method  and save as image ----------------------
 
 # Get reduction factors as compared to conservative
-MSEs <- apply(MSE_df, MARGIN = 1, FUN = sum)
-1/(MSEs["Stepped"] / MSEs["Conservative"])
-1/(MSEs["Extended"] / MSEs["Conservative"])
 
-pdf("output/plots/MSE_reduction_factors_motivating_example.pdf",
-    width = 5,
-    height = 5)
-plot.new()
-text(x = c(0,0,0),
-     y = c(1,0.5,0),
-     cex = 1.2,
-     pos = 4,
-     paste0(
-       "MSE reduction factor:",
-       c(" conservative = ", "stepped = ", " extended = "),
-       c(1,
-         round(MSEs["Conservative"] / MSEs["Stepped"],3),
-         round(MSEs["Conservative"] / MSEs["Extended"],3))
-       )
-    )
-dev.off()
+names(MSE_df) <- stringr::str_remove(names(MSE_df),"\ \\*")
+
+MSE_df$MSE <- apply(MSE_df, MARGIN = 1, FUN = sum)
+
+MSE_df <- MSE_df %>%
+  mutate(MSE_reduction_factor = MSE / max(MSE)) %>%
+  add_rownames(var = "threshold_type")
+
+file_name <- "mse_decomposition_motivating_example.csv"
+path <- here(OUTPUT_PATH, file_name)
+readr::write_csv(MSE_df, file = path)
+
 
 # 5: Return level plots --------------------------------------------------------
 
-## 5.1: Calculate point estimaes and confidence intervals ----------------------
+## 5.1: Calculate point estimates and confidence intervals ----------------------
 return_periods <- 1/(0.1^(seq(0,3,length.out = 101)))
 
-return_levels <- function(sigxi_u, u, v, return_periods_v){
-  sig_u <- sigxi_u[1]
-  xi <- sigxi_u[2]
-  sig_v <- sig_u + (v - u) * xi
+return_level(scale = mles_cons$sig_u[1], shape = mles_cons$xi[1], shift = 1.05, v = 1.45, period = return_periods)
 
-  qgpd(p = (1-1/return_periods_v), shape = xi, scale = sig_v, mu = v)
-}
+rl_cons <- mle_return_levels(mles_cons)
+rl_step <- mle_return_levels(mles_step)
+rl_xtra <- mle_return_levels(mles_xtra)
 
-return_levels(
-  sigxi = as.numeric(mles_cons[1,]),
-  u = 1.05,
-  v = 1.45,
-  return_periods_v = return_periods)
+rl_CI_cons <- return_level_confidence_interval(rl_cons)
+rl_CI_step <- return_level_confidence_interval(rl_step)
+rl_CI_xtra <- return_level_confidence_interval(rl_xtra)
 
-rl_cons <- apply(X = unname(as.matrix(mles_cons)), MARGIN = 1, FUN = return_levels, u = u, v = v_cons[1], return_periods_v = return_periods)
-rl_step <- apply(X = unname(as.matrix(mles_step)), MARGIN = 1, FUN = return_levels, u = u, v = v_cons[1], return_periods_v = return_periods)
-rl_xtra <- apply(X = unname(as.matrix(mles_xtra)), MARGIN = 1, FUN = return_levels, u = u, v = v_cons[1], return_periods_v = return_periods)
+rl_true <- qgpd(
+  p = 1 - 1/return_periods,
+  scale = sig_u + xi * (v_cons[1] - u),
+  shape = xi,
+  shift = v_cons[1])
 
-alpha <- 0.05
-rl_CI_cons <- apply(X = rl_cons, MARGIN = 1, FUN = quantile, probs = c(alpha/2, 0.5, 1-alpha/2))
-rl_CI_step <- apply(X = rl_step, MARGIN = 1, FUN = quantile, probs = c(alpha/2, 0.5, 1-alpha/2))
-rl_CI_xtra <- apply(X = rl_xtra, MARGIN = 1, FUN = quantile, probs = c(alpha/2, 0.5, 1-alpha/2))
+## 5.2: Create return level plot ----------------------------------------------
 
-rl_true <- qgpd(p = 1 - 1/return_periods, scale = sig_u + xi * (v_cons[1] - u), shape = xi, mu = v_cons[1])
+file_name <- "return_levels_motivating_example.pdf"
+path <- here(OUTPUT_PATH, file_name)
 
-lines_CI <- function(ymatrix, x, col, type,...){
-  for(i in 1:nrow(ymatrix)){
-  lines(x = x, y = ymatrix[i,], lty = type[i], col = col[i],...)
-  }
-}
-
-## 5.2: CCreate return level plot ----------------------------------------------
-
-pdf("output/plots/return_levels_motivating_example.pdf", width = 7, height = 5)
+pdf(path, width = 7, height = 5)
 par(mar = c(4.1,4.1,1.1,1.1))
 plot(
-  y = rbind(rl_CI_cons, rl_CI_step, rl_CI_xtra),
-  x = rep(return_periods, each = 9),
-  col = rep(1:3, each = 3),
+  y = c(rl_CI_cons$upper_limit, rl_CI_cons$lower_limit),
+  x = rep(return_periods, 2),
   log = 'x',
   type = 'n',
   ylab = "conditional return level",
   xlab = "return period",
   bty = "n")
-lines(x = return_periods, y = rl_true, col = 1, lwd = 2)
-lines_CI(x = return_periods, ymatrix = rl_CI_cons, col = rep("grey30",3), type = c(2,2,2), lwd = 2)
-lines_CI(x = return_periods, ymatrix = rl_CI_xtra, col = rep("grey70",3), type = c(3,3,3), lwd = 2)
-lines_CI(x = return_periods, ymatrix = rl_CI_step, col = rep("grey50",3), type = c(4,4,4), lwd = 2)
+CI_lines(rl_CI_cons, col = "grey30", lwd = 2, lty = 2)
+CI_lines(rl_CI_xtra, col = "grey70", lwd = 2, lty = 3)
+CI_lines(rl_CI_step, col = "grey50", lwd = 2, lty = 4)
+lines(x = return_periods, y = rl_true, col = "black", lwd = 2)
 legend('topleft',
        c("True","Conservative", "Extended", "Stepped"),
-       col = c("black","grey30", "grey70", "grey50"),
+       col = c("black","grey40", "grey60", "grey80"),
        lty = c(1,2,3,4),
        lwd = 2,
        cex = 1.2,
