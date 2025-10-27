6a3c74e785efce279bb266a0f52cb43bc871c8ea
Zak D Varty <44709288+zakvarty@users.noreply.github.io>
2025-10-27 10:11:52 +0000

    clean and lint helpers

--- 03_helpers.R
+++ 03_helpers.R
@@ -1,13 +1,15 @@
 #helper functions for motivating example
 
-compare_MSEs <- function(MSEs, bar_colors = NULL, bar_names = NULL, legend_position = 'topleft', legend_text = letters[seq_along(MSEs)], legend_cex = 1,...){
+compare_MSEs <- function(MSEs, bar_colors = NULL, bar_names = NULL, legend_position = 'topleft', legend_text = letters[seq_along(MSEs)], legend_cex = 1, ...){
 
   # Function to plot many MSE decompositions side-by-side
   heights <- NULL
-  if(is.null(bar_names)){ bar_names <- c('bais^2(sigma)', 'bais^2(xi)', 'var(sigma)','var(xi)')}
-  if(is.null(bar_colors)){ bar_colors <- grey(ppoints(length(MSEs))) }
+  if (is.null(bar_names)) {
+    bar_names <- c('bais^2(sigma)', 'bais^2(xi)', 'var(sigma)','var(xi)')
+    }
+  if (is.null(bar_colors)) { bar_colors <- grey(ppoints(length(MSEs))) }
 
-  for(i in seq_along(MSEs)){
+  for (i in seq_along(MSEs)) {
     heights_i <- c(MSEs[[i]]$sq_bias, MSEs[[i]]$variance)
     heights <- rbind(heights,heights_i)
   }
@@ -20,14 +22,15 @@ compare_MSEs <- function(MSEs, bar_colors = NULL, bar_names = NULL, legend_posit
   invisible(heights_df)
 }
 
-
-mle_return_levels <- function(mle_obj, ...){
+# Calculate conditional return levels for excesses of V for GPD models fitted
+# to excesses of u.
+mle_return_levels <- function(mle_obj, shift, v){
   out <- purrr::pmap(
     .l = list(scale = mle_obj$sig_u, shape = mle_obj$xi),
     .f = return_level,
     period = return_periods,
-    shift = u,
-    v = v_cons[1])
+    shift = shift,
+    v = v)
 
   # reformatting output
   out <- do.call(rbind, out)
@@ -47,10 +50,13 @@ mle_return_levels <- function(mle_obj, ...){
 
 
 
-return_level_confidence_interval<- function(rl_obj, alpha = 0.05){
+return_level_confidence_interval <- function(rl_obj, alpha = 0.05){
+  as_numeric_factor <- function(x) {as.numeric(levels(x))[x]}
+
   rl_obj %>%
     mutate(return_period = as.factor(return_period)) %>%
     summarise(
+      #return_period = return_period,
       lower_limit = unname(quantile(return_level, alpha / 2)),
       median = unname(quantile(return_level, 0.5)),
       upper_limit = unname(quantile(return_level, 1 - alpha / 2)),
@@ -58,7 +64,7 @@ return_level_confidence_interval<- function(rl_obj, alpha = 0.05){
     mutate(return_period = as_numeric_factor(return_period))
 }
 
-CI_lines <- function (CI_obj, ...) {
+CI_lines <- function(CI_obj, ...){
   lines(x = CI_obj$return_period, y = CI_obj$median, ...)
   lines(x = CI_obj$return_period, y = CI_obj$lower_limit, ...)
   lines(x = CI_obj$return_period, y = CI_obj$upper_limit, ...)
