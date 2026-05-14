072a14b6199c437f86478f53364bfdb633cdc21f
Zak D Varty <44709288+zakvarty@users.noreply.github.io>
2026-05-14 08:41:41 +0100

    Add sf dependency and update arg name

--- 01_make_earthquake_catalogue.R
+++ 01_make_earthquake_catalogue.R
@@ -1,10 +1,11 @@
 # 01_make_earthquake_catalogue.R -----------------------
 
-DOWNLOAD_NEW_JSON = FALSE
-OUTPUT_PATH = "00_data/eathquake_catalogues/"
+DOWNLOAD_NEW_JSON = TRUE
+OUTPUT_PATH = "00_data/earthquake_catalogues/"
 
 library(threshold)
 library(stringr)
+library(sf)
 
 if (DOWNLOAD_NEW_JSON) { write_groningen_json(file_path = OUTPUT_PATH) }
 
@@ -12,8 +13,8 @@ if (DOWNLOAD_NEW_JSON) { write_groningen_json(file_path = OUTPUT_PATH) }
 json_path <- threshold:::latest_json_file(OUTPUT_PATH)
 csv_path  <- stringr::str_replace(string = json_path, ".json", ".csv")
 
-gf_cat <- threshold::read_JSON_cat(
-  JSON_path = json_path,
+gf_cat <- threshold::read_json_cat(
+  json_path = json_path,
   field_outline_with_buffer = threshold::gfo_buffer_1000)
 
 if (DOWNLOAD_NEW_JSON) {
